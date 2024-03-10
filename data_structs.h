eeb99ab6bbcb4fad0f7e2bb3d9e03a9d2df8a100
Ben Xiao <benssiao@gmail.com>
2024-03-10 16:47:31 -0700

    added necessary data struct headers and changed screen display of game.

---
+++ data_structs.h
@@ -0,0 +1,11 @@
+
+
+int hash(int);
+typedef struct hash_table {
+    int table_size;
+
+} hash_table;
+typedef struct linked_list {
+    int val;
+    struct linked_list *next;
+} linked_list;
--- snake_game.c
+++ snake_game.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+
+#define SCREEN_DIMENSION 45
+#define VERTICAL_SIZE 25
 void move_input(void);
 void generate_board(void);
 
 int main(int argc, char *argv[]) {
 
-    char vert_frame[51], horizontal_frame[51];
-    memset(vert_frame, '_', 50);
-    vert_frame[50] = '\0';
-    memset(horizontal_frame, ' ', 49);
+    char vert_frame[SCREEN_DIMENSION+1], horizontal_frame[SCREEN_DIMENSION+1];
+    memset(vert_frame, '_', SCREEN_DIMENSION);
+    vert_frame[SCREEN_DIMENSION] = '\0';
+    memset(horizontal_frame, ' ', SCREEN_DIMENSION);
     horizontal_frame[0] = '|';
-    horizontal_frame[49] = '|';
-    horizontal_frame[50] = '\0';
+    horizontal_frame[SCREEN_DIMENSION-1] = '|';
+    horizontal_frame[SCREEN_DIMENSION] = '\0';
     for(;;) {
 
         printf("%s\n", vert_frame);
-        for(int i=0; i<50; i++) {
+        for(int i=0; i<VERTICAL_SIZE; i++) {
             printf("%s\n", horizontal_frame);
             
         }
         printf("%s\n", vert_frame);
-        fflush(stdout);
+        usleep(500000);
+        system("clear"); 
+
     }
     return 0;
 }
